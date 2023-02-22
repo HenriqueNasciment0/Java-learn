088314c60d23249522c358850c4fa781eaf71982
HenriqueNasciment0 <you@example.com>
2023-02-22 13:01:16 -0300

    capgemini - while

---
+++ capgemini-bootcamp/aula4-while/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int total = 10;
+
+        while (total > 0) {
+            System.out.println(total);
+            total = total - 1;
+        }
+
+    }
+}
