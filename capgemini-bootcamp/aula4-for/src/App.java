5cfaab98a039799e7b7598fbbb18cac8fd95a0f5
HenriqueNasciment0 <you@example.com>
2023-02-22 12:34:51 -0300

    campgemini - uso do for básico

---
+++ capgemini-bootcamp/aula4-for/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int i;
+
+        for(i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+
+    }
+}
